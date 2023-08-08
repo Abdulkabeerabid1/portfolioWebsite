a9c9a14d3bfe99226c11e5c9cd3ffe93ccc32d1b
abdul kabeer abid <kabeer.abid19@gmail.com>
2023-08-08 21:37:49 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FirstComp from "./FirstComp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<FirstComp />
+	</React.StrictMode>
+);
