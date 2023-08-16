39020081acf8ac618865ef2c9bbabdce07a85fc5
abdul kabeer abid <kabeer.abid19@gmail.com>
2023-08-16 18:54:46 +0500

    changingss

--- src/FirstComp.jsx
+++ src/FirstComp.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function FirstComp() {
   return (
     <div>
-      <h1>My name is abdul kabeer abidasdasd</h1>
+      <h1>My name is abdul kabeer asdasdasd</h1>
     </div>
   );
 }
